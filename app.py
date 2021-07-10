d4b394f10418aa8980818c8a529e3b12c0c3fe10
mizphses <fuminori@mizphses.com>
2021-07-11 02:12:44 +0900

    認証とか

--- app.py
+++ app.py
@@ -1,77 +1,71 @@
-from flask import Flask, jsonify, request, abort
-from flask_jwt import JWT, jwt_required, current_identity
-import psycopg2
-from dotenv import load_dotenv
-from werkzeug.security import generate_password_hash, check_password_hash
 import os
-from os.path import join, dirname
+from os.path import dirname, join
+
+from dotenv import load_dotenv
+from flask import Flask, abort, jsonify, request
+from flask_jwt import JWT, current_identity, jwt_required
+
+# Modules
+from auth import *
+from connection import *
 
 app = Flask(__name__)
 
 app.config["JSON_AS_ASCII"] = False
+app.config["JWT_SECRET_KEY"] = os.environ.get('JWT_SECRET')
 load_dotenv(verbose=True)
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-# Connect to PostgreSQL
-def get_connection():
-    dsn = os.environ.get('DATABASE_URL')
-    return psycopg2.connect(dsn)
-conn = get_connection()
-cur = conn.cursor()
-
-users = []
-
 # Auth
-class User(object):
-    def __init__(self, id, username, password):
-        self.id = id
-        self.username = username
-        self.password = password
+jwt = JWT(app, authenticate, identity)
 
-    def __str__(self):
-        return "User(id='%s')" % self.id
-
-
-
-# API
-@app.route("/setup")
-def setup():
-    with conn.cursor() as cur:
-        cur.execute("CREATE TABLE IF NOT EXISTS users (id serial PRIMARY KEY, username varchar UNIQUE, useremail varchar UNIQUE, password_digest varchar);")
-    return jsonify({"Message":"Your setup was successfully finished"})
+# root, プレイエリア外
+@app.route("/test")
+def testroute():
+    print(user_fetch())
+    return jsonify({"a":"hi"})
 
 @app.route("/")
 def root():
-    return jsonify({"status":"403 Forbidden", "message":"プレイエリアの外です。"}), 403
+    return jsonify({"status":"403 Forbidden", "message":"メッセージはでないはずだよ"}), 403
 
+
+
+# # ユーザ登録
 @app.route("/register_user", methods=["post"])
-def post():
+def post_user():
     username = request.json['username']
     useremail = request.json['useremail']
     password = request.json['userpw']
+    namae = request.json['namae']
     password_hash = generate_password_hash(password, method='sha256')
     with conn.cursor() as cur:
-        cur.execute('INSERT INTO users (username, useremail, password_digest) VALUES (%s, %s, %s)', (username, useremail, password_hash))
+        cur.execute('INSERT INTO users (username, useremail, password_digest, namae) VALUES (%s, %s, %s, %s)', (username, useremail, password_hash, namae))
     conn.commit()
     return jsonify({"message":"200 OK"}), 200
 
-@app.route("/userlist")
-def userlist():
-    cur.execute('SELECT id, username, useremail FROM users;')
-    results = cur.fetchall()
-    return jsonify({"User":results})
+# # ユーザ更新
+@app.route("/alter_user", methods=["put"])
+def alter_user():
+    username = request.json['username']
+    useremail = request.json['useremail']
+    password = request.json['userpw']
+    namae = request.json['namae']
+    password_hash = generate_password_hash(password, method='sha256')
+    with conn.cursor() as cur:
+        cur.execute('UPDATE users SET username=%s, useremail=%s, password_digest=%s, namae=%s WHERE username = %s', (username, useremail, password_hash, namae, username))
+    conn.commit()
+    return jsonify({"message":"200 OK"}), 200
 
-@app.errorhandler(404)
-def page_not_found(error):
-    return jsonify(
-        {
-            "Status": "Error",
-            "Code": "404",
-            "Message": "Page or endpoint not found"
-        }
-    ), 404
+
+
+
+# # エラーハンドリング
+@app.route("/500")
+def hello():
+    abort(500, 'hello abort')
 
 @app.route("/418")
 def index():
@@ -82,16 +76,24 @@ def index():
             "Message": "The requested entity body is short and stout. Tip me over and pour me out."
         }), 418
 
-@app.route("/500")
-def hello():
-    abort(500, 'hello abort')
-
-# Error Hundler
-
 @app.errorhandler(500)
 def error_500(e):
     return jsonify({'message': 'internal server error'}), 500
 
+@app.errorhandler(404)
+def error_404(e):
+    return jsonify({'message': 'Page not found'}), 500
+
+# # Security Area
+
+# テスト用
+@app.route('/protected')
+@jwt_required()
+def protected():
+    return '%s' % current_identity
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8888)
+    if os.environ.get('IS_DEBUG') == "True":
+        app.run(debug=True, port=8888)
+    else:
+        app.run(debug=False, port=8888)
